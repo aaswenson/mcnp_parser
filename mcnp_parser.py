789b7fea6d11a7c668ac76a77ce79e282fb3e074
Mouginot B <mouginot.baptiste@gmail.com>
2017-10-17 14:54:17 -0500

    fig update

--- mcnp_parser.py
+++ mcnp_parser.py
@@ -15,8 +15,8 @@ def read_output(input):
             if "total" in line:
                 total = float(line.split()[1])
                 store = False
-                for i in range(len(spectra)):
-                    spectra[i][1] *= 1./total
+   #             for i in range(len(spectra)):
+   #                 spectra[i][1] *= 1./total
    #                 spectra[i][2] *= 1./total
                 spectrum.append(spectra)
                 total = 0
